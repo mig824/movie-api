9c76f3986ad4c26e17fb2578781a63cba3ee7f0e
mig824 <migmic93@gmail.com>
2020-11-26 15:53:11 -0800

    (add) mongo connection

--- src/api/server.js
+++ src/api/server.js
@@ -1,16 +1,34 @@
 const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./gql/schema');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+const typeDefs = require('./gql/schema');
 const resolvers = require('./gql/resolvers');
 const MovieAPI = require('./gql/data-sources/movie-api');
-const API_KEY = process.env.API_KEY;
+const MovieDB = require('./gql/data-sources/movie-db');
+const MovieModel = require('./db/movie-model');
+const { API_KEY, MONGO_URI, PORT } = process.env;
+
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'Liked_Movies',
+    });
+    console.log('Mongo Connection Status: [ OK ]\n');
+  } catch (err) {
+    console.log('Mongo Connection Status: [ FAIL ]\n');
+    throw new Error(err);
+  }
+})();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
     movieAPI: new MovieAPI(),
+    movieDB: new MovieDB(MovieModel),
   }),
   context: () => ({
     apiKey: API_KEY,
@@ -18,6 +36,6 @@ const server = new ApolloServer({
 });
 
 (async () => {
-  const { url } = await server.listen();
+  const { url } = await server.listen({ port: PORT });
   console.log(`\nGraphQL IDE --> ${url}\n`);
 })();
