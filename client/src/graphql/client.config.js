1e6dd24d28b48876ebd6fcfec7e758dbe0851b62
mig824 <migmic93@gmail.com>
2020-11-27 13:21:26 -0800

    (add) apollo client config

---
+++ client/src/graphql/client.config.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const GQL_URL = process.env.GQL_URL || 'http://localhost:3333/graphql';
+
+const client = new ApolloClient({
+  uri: GQL_URL,
+  cache: new InMemoryCache(),
+});
+
+export default client;
